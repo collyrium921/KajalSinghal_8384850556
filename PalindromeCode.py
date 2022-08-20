32256c330fe17720f8316dc1fcd54e8bef0a5374
Kajal Singhal <kajusinghal921@gmail.com>
2022-08-20 15:28:54 +0530

    palindrome code added

---
+++ PalindromeCode.py
@@ -0,0 +1,14 @@
+#get the input of the number
+num = int(input("Enter a number"))
+#assign the number to a temporary varaible
+temp = num
+reverse_num =0;
+#reverse the digits of the number
+while temp>0:
+    digit = num%10
+    reverse_num = reverse_num*10+digit
+    temp=temp//10
+if(num == reverse_num):
+    return 'Yes'
+else:
+    return 'No'     
